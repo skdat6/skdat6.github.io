40d928b416a628b23e838bf127b1cd86041620c6
skdat6 <dobretheo@gmail.com>
2022-08-12 17:24:16 +0300

    added index, css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// BUTTON stuff
+var btn = document.getElementById('contact-button');
+
+
+function buttonClicked() {
+  window.open("mailto:dobretheo@gmail.com");
+}
+
+
+btn.addEventListener("click", buttonClicked);
